bdab158f17cc1122a48d1553f26c4828fd04e503
sirtrainsalot <sir.trainsalot@gmail.com>
2020-02-04 08:15:26 +0100

    Added Pets and Contact info to owners on startup. Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,11 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Owner extends Person {
 
     private String address;
     private String city;
+    private String telephone;
+    private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
         return address;
@@ -39,7 +42,6 @@ public class Owner extends Person {
         this.pets = pets;
     }
 
-    private String telephone;
-    private Set<Pet> pets;
+
 
 }
